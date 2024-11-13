39ed5cfb4faa271ccffd17c7a551e9860f634a56
Caro405 R <rios_carolf@javeriana.edu.co>
2024-11-13 05:41:54 +0000

    cambios varios

--- demo/src/main/java/com/example/demo/Controladores/RegistroController.java
+++ demo/src/main/java/com/example/demo/Controladores/RegistroController.java
@@ -1,21 +1,18 @@
 package com.example.demo.Controladores;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.Servicio.CuentaServicio;
 
-@RestController
+@Controller
 public class RegistroController {
 
     private final CuentaServicio cuentaServicio;
 
-    @Autowired
     public RegistroController(CuentaServicio cuentaServicio) {
         this.cuentaServicio = cuentaServicio;
     }
